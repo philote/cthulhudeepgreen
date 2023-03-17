97f7870c97cdd60d4d9f3aefd4810a9e2880118c
Joseph Hopson <727468+philote@users.noreply.github.com>
2023-03-16 19:29:29 -0600

    starting work on the character sheet

--- module/sheets/item-sheet.mjs
+++ module/sheets/item-sheet.mjs
@@ -8,8 +8,8 @@ export class CthulhuDeepGreenItemSheet extends ItemSheet {
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
       classes: ["cthulhudeepgreen", "sheet", "item"],
-      width: 520,
-      height: 480,
+      width: 310,
+      height: 620,
       tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }]
     });
   }
@@ -18,11 +18,11 @@ export class CthulhuDeepGreenItemSheet extends ItemSheet {
   get template() {
     const path = "systems/cthulhudeepgreen/templates/item";
     // Return a single sheet for all item types.
-    // return `${path}/item-sheet.html`;
+    return `${path}/item-sheet.html`;
 
     // Alternatively, you could use the following return statement to do a
     // unique item sheet by type, like `weapon-sheet.html`.
-    return `${path}/item-${this.item.type}-sheet.html`;
+    // return `${path}/item-${this.item.type}-sheet.html`;
   }
 
   /* -------------------------------------------- */
