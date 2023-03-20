8a1884091937f9f9c56cf0bab0a71de41c7baeae
Joseph Hopson <727468+philote@users.noreply.github.com>
2023-03-19 18:48:46 -0600

    got Failure and self care rolls working

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -1,23 +1,6 @@
 export const CDG = {};
 
-/**
- * The set of Ability Scores used within the sytem.
- * @type {Object}
- */
-//  CDG.abilities = {
-//   "str": "CDG.AbilityStr",
-//   "dex": "CDG.AbilityDex",
-//   "con": "CDG.AbilityCon",
-//   "int": "CDG.AbilityInt",
-//   "wis": "CDG.AbilityWis",
-//   "cha": "CDG.AbilityCha"
-// };
-
-// CDG.abilityAbbreviations = {
-//   "str": "CDG.AbilityStrAbbr",
-//   "dex": "CDG.AbilityDexAbbr",
-//   "con": "CDG.AbilityConAbbr",
-//   "int": "CDG.AbilityIntAbbr",
-//   "wis": "CDG.AbilityWisAbbr",
-//   "cha": "CDG.AbilityChaAbbr"
-// };
+CDG.DarkDieColor = "#A62424";
+CDG.StressColor = "#A62424";
+CDG.InsightColor = "#2ba624";
+CDG.BaseColor = "#000000";
--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -39,14 +39,11 @@ export class CthulhuDeepGreenActorSheet extends ActorSheet {
 
     // Prepare character data and items.
     if (actorData.type == 'character') {
-      this._prepareItems(context);
       this._prepareCharacterData(context);
     }
 
     // Prepare NPC data and items.
-    if (actorData.type == 'npc') {
-      this._prepareItems(context);
-    }
+    // if (actorData.type == 'npc') {}
 
     // Add roll data for TinyMCE editors.
     context.rollData = context.actor.getRollData();
@@ -65,35 +62,17 @@ export class CthulhuDeepGreenActorSheet extends ActorSheet {
     
   }
 
-  /**
-   * Organize and classify Items for Character sheets.
-   *
-   * @param {Object} actorData The actor to prepare.
-   *
-   * @return {undefined}
-   */
-  _prepareItems(context) {
-    // Initialize containers.
-  }
-
   /* -------------------------------------------- */
 
   /** @override */
   activateListeners(html) {
     super.activateListeners(html);
 
-    // Render the item sheet for viewing/editing prior to the editable check.
-    html.find('.item-edit').click(ev => {
-      const li = $(ev.currentTarget).parents(".item");
-      const item = this.actor.items.get(li.data("itemId"));
-      item.sheet.render(true);
-    });
-
     // -------------------------------------------------------------
     // Everything below here is only needed if the sheet is editable
     if (!this.isEditable) return;
 
-    // Rollable abilities.
+    // Rollable.
     html.find('.rollable').click(this._onRoll.bind(this));
   }
 
@@ -107,6 +86,25 @@ export class CthulhuDeepGreenActorSheet extends ActorSheet {
     const element = event.currentTarget;
     const dataset = element.dataset;
 
+    // Handle rolls.
+    if (dataset.rollType) {
+      switch (dataset.rollType) {
+        case 'selfcare': 
+        case 'failure': {
+          this.simpleRoll(dataset.rollType);
+          return;
+        }
+        case 'darkdie': {
+          // TODO Dialog to ask if you risk stress, insight or both
+        }
+        case 'riskything':
+        default: { 
+          // TODO Dialog
+          return;
+        }
+      }
+    }
+
     // Handle rolls that supply the formula directly.
     if (dataset.roll) {
       let label = dataset.label ? `[ability] ${dataset.label}` : '';
@@ -118,6 +116,99 @@ export class CthulhuDeepGreenActorSheet extends ActorSheet {
       });
       return roll;
     }
+  }
+
+  // ----------------------
+  // Dice rolling functions
+  // ----------------------
+  
+  getDiceForOutput(dieNumber, colorHex) {
+    switch (dieNumber) {
+        case "1":
+            return `<i class="fas fa-dice-one" style="color:${colorHex}; font-size: 2em;"></i>`;
+        case "2":
+            return `<i class="fas fa-dice-two" style="color:${colorHex}; font-size: 2em;"></i>`;
+        case "3":
+            return `<i class="fas fa-dice-three" style="color:${colorHex}; font-size: 2em;"></i>`;
+        case "4":
+            return `<i class="fas fa-dice-four" style="color:${colorHex}; font-size: 2em;"></i>`;
+        case "5":
+            return `<i class="fas fa-dice-five" style="color:${colorHex}; font-size: 2em;"></i>`;
+        case "6":
+            return `<i class="fas fa-dice-six" style="color:${colorHex}; font-size: 2em;"></i>`;
+        default:
+            console.error("Error in the getDiceForOutput, bad die number used.");
+    }
+  }
+
+  getWordInsightWithFormatting() {
+    return `<b style="color: ${CONFIG.CDG.InsightColor}"><i>${game.i18n.localize("CDG.Insight")}</i></b>`;
+  }
+
+  getWordStressWithFormatting() {
+    return `<b style="color: ${CONFIG.CDG.StressColor}"><i>${game.i18n.localize("CDG.Stress")}</i></b>`;
+  }
+  
+  // ------------
+  // Simple Rolls
+  // ------------
+
+  failureChatContent(diceOutput) {
+    return `
+        <p>
+          <span style="font-size: 1.5em;">
+            ${game.i18n.localize('CDG.FailureRoll')} 
+          </span> 
+          ${diceOutput}
+        </p>
+        <hr>
+        ${game.i18n.localize('CDG.FailureRollContent')}
+    `;
+  }
+
+  selfCareChatContent(diceNumber) {
+    let previousStress = duplicate(this.actor.system.stress.value);
+    const rollValue = +diceNumber;
+    let newStress = previousStress - rollValue;
+    newStress = newStress < 0 ? 0 : newStress;
+
+    // update Stress
+    this.actor.system.stress.value = newStress;
+    this.actor.update({"system.stress.value": newStress});
+
+    let selfCareMessage = `<p><span style="font-size: 1.5em;">${game.i18n.localize('CDG.SelfCareRoll')} </span>${this.getDiceForOutput(diceNumber, CONFIG.CDG.BaseColor)}</p><hr>`;
+    return selfCareMessage.concat(game.i18n.format('CDG.SelfCareRollContent', {stress1: this.getWordStressWithFormatting(), previousstress: previousStress, newstress: newStress, stress2: this.getWordStressWithFormatting()}));
+  }
+
+  async simpleRoll(rollType) {
+    let simpleRoll = await new Roll('1d6').evaluate({ async: true });
+    let chatContentMessage = "";
+
+    switch (rollType) {
+      case 'selfcare': {
+        chatContentMessage = this.selfCareChatContent(simpleRoll.result);
+        break;
+      }
+      case 'failure': {
+        chatContentMessage = this.failureChatContent(this.getDiceForOutput(simpleRoll.result, CONFIG.CDG.BaseColor));
+        break;
+      }
+      default: { 
+        console.error("Error in the simpleRoll, bad die rollType used.");
+        return;
+      }
+    }
+
+    const user = game.user.id;
+    const speaker = ChatMessage.getSpeaker({ actor: this.actor });
+    const rollMode = game.settings.get('core', 'rollMode');
+
+    ChatMessage.create({
+      user: user,
+      speaker: speaker,
+      rollMode: rollMode,
+      content: chatContentMessage
+    });
   }
 
 }
